5b9042fd1d1c3dacf6ffb43254fb8330a0dfebbc
Felixniebisch <131880782+Felixniebisch@users.noreply.github.com>
2024-04-15 15:52:41 +0200

    Update distribute.js

--- distribute.js
+++ distribute.js
@@ -1,4 +1,4 @@
-document.getElementById('randomLinkButton').addEventListener('click', function() {
+document.getElementById('accessButton').addEventListener('click', function() {
     var urls = [
         'https://forms.office.com/Pages/ShareFormPage.aspx?id=-aZLWjH1Mk-UZzmPGead5BsZ3PLJiRJKubeXx0D-61FUQkZEOTJYM0UxQ05JSTFXS1BIRE9HM1I2QS4u&sharetoken=DpJRPJSQN26yElCTaBNZ',
         'https://forms.office.com/Pages/ShareFormPage.aspx?id=-aZLWjH1Mk-UZzmPGead5BsZ3PLJiRJKubeXx0D-61FUMlo4T1lBSjRYOFdZOTdENDFaRFJWM0szQS4u&sharetoken=DpJRPJSQN26yElCTaBNZ'
