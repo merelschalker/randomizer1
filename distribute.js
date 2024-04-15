0c83630797a719e82d14a955e51e84458a5938c2
Felixniebisch <131880782+Felixniebisch@users.noreply.github.com>
2024-04-15 15:39:15 +0200

    Update distribute.js

--- distribute.js
+++ distribute.js
@@ -1,5 +1,11 @@
 document.getElementById('randomLinkButton').addEventListener('click', function() {
-    var urls = ['https://forms.office.com/Pages/ShareFormPage.aspx?id=-aZLWjH1Mk-UZzmPGead5BsZ3PLJiRJKubeXx0D-61FUQkZEOTJYM0UxQ05JSTFXS1BIRE9HM1I2QS4u&sharetoken=DpJRPJSQN26yElCTaBNZ', 'https://forms.office.com/Pages/ShareFormPage.aspx?id=-aZLWjH1Mk-UZzmPGead5BsZ3PLJiRJKubeXx0D-61FUMlo4T1lBSjRYOFdZOTdENDFaRFJWM0szQS4u&sharetoken=DpJRPJSQN26yElCTaBNZ'];
-    var randomIndex = Math.floor(Math.random() * urls.length); // Randomly selects 0 or 1
-    window.location.href = urls[randomIndex]; // Redirects user to randomly selected link
+    var urls = [
+        'https://forms.office.com/Pages/ShareFormPage.aspx?id=-aZLWjH1Mk-UZzmPGead5BsZ3PLJiRJKubeXx0D-61FUQkZEOTJYM0UxQ05JSTFXS1BIRE9HM1I2QS4u&sharetoken=DpJRPJSQN26yElCTaBNZ',
+        'https://forms.office.com/Pages/ShareFormPage.aspx?id=-aZLWjH1Mk-UZzmPGead5BsZ3PLJiRJKubeXx0D-61FUMlo4T1lBSjRYOFdZOTdENDFaRFJWM0szQS4u&sharetoken=DpJRPJSQN26yElCTaBNZ'
+    ];
+    var currentIndex = parseInt(localStorage.getItem('lastIndex')) || 0;  // Get the last index if it exists, or start with 0
+    var nextIndex = (currentIndex + 1) % urls.length;  // Calculate the next index: 0 if the current is 1, 1 if the current is 0
+    
+    window.location.href = urls[nextIndex];  // Redirect to the next URL
+    localStorage.setItem('lastIndex', nextIndex);  // Update the last index in local storage
 });
