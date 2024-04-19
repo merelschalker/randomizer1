ba27a2ef1ea0347985cc5f967a4464c7a6125b0f
Felixniebisch <131880782+Felixniebisch@users.noreply.github.com>
2024-04-19 13:16:37 +0200

    Update distribute.js
    Update back to randomizer

--- distribute.js
+++ distribute.js
@@ -1,11 +1,5 @@
-document.getElementById('accessButton').addEventListener('click', function() {
-    var urls = [
-        'https://forms.office.com/e/TGhmgz0P95',
-        'https://forms.office.com/e/Gm8z2fDfm1'
-    ];
-    var currentIndex = parseInt(localStorage.getItem('lastIndex')) || 0;  // Get the last index if it exists, or start with 0
-    var nextIndex = (currentIndex + 1) % urls.length;  // Calculate the next index: 0 if the current is 1, 1 if the current is 0
-    
-    window.location.href = urls[nextIndex];  // Redirect to the next URL
-    localStorage.setItem('lastIndex', nextIndex);  // Update the last index in local storage
+document.getElementById('randomLinkButton').addEventListener('click', function() {
+    var urls = ['https://forms.office.com/e/TGhmgz0P95', 'https://forms.office.com/e/Gm8z2fDfm1'];
+    var randomIndex = Math.floor(Math.random() * urls.length); // Randomly selects 0 or 1
+    window.location.href = urls[randomIndex]; // Redirects user to randomly selected link
 });
