ad65c58e7552450419fc8470dbdce7d2022c07a7
merelschalker <167547125+merelschalker@users.noreply.github.com>
2024-04-19 16:45:10 +0200

    Update distribute.js
    updated to three links

--- distribute.js
+++ distribute.js
@@ -1,5 +1,5 @@
 document.getElementById('accessButton').addEventListener('click', function() {
-    var urls = ['https://forms.office.com/e/TGhmgz0P95', 'https://forms.office.com/e/Gm8z2fDfm1'];
+    var urls = ['Link1', 'Link2', " 'Link3']; #include your links here, just put in a thirs link, divided by a comma. T
     var randomIndex = Math.floor(Math.random() * urls.length); // Randomly selects 0 or 1
     window.location.href = urls[randomIndex]; // Redirects user to randomly selected link
 });
