611da3240e7188094412b83545023533aefb112f
Felixniebisch <131880782+Felixniebisch@users.noreply.github.com>
2024-04-19 13:20:43 +0200

    Update distribute.js
    changes button

--- distribute.js
+++ distribute.js
@@ -1,4 +1,4 @@
-document.getElementById('randomLinkButton').addEventListener('click', function() {
+document.getElementById('accessButton').addEventListener('click', function() {
     var urls = ['https://forms.office.com/e/TGhmgz0P95', 'https://forms.office.com/e/Gm8z2fDfm1'];
     var randomIndex = Math.floor(Math.random() * urls.length); // Randomly selects 0 or 1
     window.location.href = urls[randomIndex]; // Redirects user to randomly selected link
